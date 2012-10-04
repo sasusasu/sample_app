d7ac75b65103dd52520af7c40513e166f84e35d8
Sasu Keränen <sasu@iwa.fi>
2012-10-04 11:10:20 +0300

    Finish sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper # to get helper methods for session management
 end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,29 @@
+module SessionsHelper
+	def sign_in(user)
+		# cookies.permanent is equal to:
+		# cookies[:remember_token] = { value: user.remember_token, expires: 20.years.from_now.utc }
+		cookies.permanent[:remember_token] = user.remember_token
+		self.current_user = user
+	end
+
+	def signed_in?
+		!current_user.nil?
+	end
+
+	def sign_out
+		self.current_user = nil
+		cookies.delete(:remember_token)
+	end
+	
+	# note the assignment (=), self.current_user=... is automatically converted to current_user=(...)
+	def current_user=(user)
+		@current_user = user
+	end
+
+	def current_user
+		# remember the ||= a.k.a. "or equals"...sets the @current_user instance variable
+		# but only if it is undefined!
+		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
+	end
+
+end
